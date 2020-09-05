a0fb5533937b22272a44be8d6b27b8bc3a2e1a1e
dmill166 <dmill166@msudenver.edu>
2020-09-04 20:38:27 -0600

    uploaded files from hector

--- src/Inventory.java
+++ src/Inventory.java
@@ -36,7 +36,65 @@ public class Inventory {
         }
         //System.out.println(inventory.get(1).toString());
         sc.close();
-    }
+    }// End loadInventory
+
+    public int getQuantity(String id){
+        for(int i = 0; i < inventory.size(); i++){
+            if(inventory.get(i).getProductID().equals(id))
+                return inventory.get(i).getQuantity();
+        }
+        return 0;
+    }// End getQuantity
+
+    public double getSalePrice(String id){
+        for(int i = 0; i < inventory.size(); i++){
+            if(inventory.get(i).getProductID().equals(id))
+                return inventory.get(i).getSalePrice();
+        }
+        return 0;
+    }// End getQuantity
+
+    public String getSupplierID(String id){
+        for(int i = 0; i < inventory.size(); i++){
+            if(inventory.get(i).getProductID().equals(id))
+                return inventory.get(i).getSupplierID();
+        }
+        return "";
+    }// End getSupplierID
+
+    public double getWholePrice(String id){
+        for(int i = 0; i < inventory.size(); i++){
+            if(inventory.get(i).getProductID().equals(id))
+                return inventory.get(i).getWholesale();
+        }
+        return 0;
+    }//End getWholePrice
+
+    public void setSalePrice(String id, double price){
+        for(int i = 0; i < inventory.size(); i++){
+            if(inventory.get(i).getProductID().equals(id))
+                inventory.get(i).setSalePrice(price);
+        }
+    }// End setSalePrice
+
+    public void setWholeSalePrice(String id, double price){
+        for(int i = 0; i < inventory.size(); i++){
+            if(inventory.get(i).getProductID().equals(id))
+                inventory.get(i).setWholesale(price);
+        }
+    }// End setWholeSalePrice
+
+    public void setQuantity(String id, int quantity){
+        for(int i = 0; i < inventory.size(); i++){
+            if(inventory.get(i).getProductID().equals(id))
+                inventory.get(i).setQuantity(quantity);
+        }
+    }// End setQuantity
+
+        
+
+
+
 
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -52,4 +110,4 @@ public class Inventory {
 
 
 
-}
+}// End Class
