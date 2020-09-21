71cbe089255d6dc4e3cf6009f29811c42a1c8e90
dmill166 <70177994+dmill166@users.noreply.github.com>
2020-09-20 19:02:34 -0600

    Revert "Update"

--- src/InventoryApp.java
+++ src/InventoryApp.java
@@ -13,60 +13,28 @@ public class InventoryApp {
     private JButton buyButton;
     private final Inventory inventory;
 
-    private void notFound() {
+    private void notFound(){
         JOptionPane.showMessageDialog(null, "Error: Product Not Found");
-
     }
 
-
-    private String obtainProductID() {
-        String s = null;
-        try {
-            s = JOptionPane.showInputDialog("Enter product ID").toUpperCase();
-        } catch (NullPointerException ignore) {
-        } finally {
-            return s;
-        }
+    private String obtainProductID(){
+        return JOptionPane.showInputDialog("Enter product ID").toUpperCase();
     }
 
-    private int obtainQuantity() {
-        Integer i = null;
-        try {
-            i = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter quantity"));
-        } catch (NullPointerException ignore) {
-        } finally {
-            return i;
-        }
+    private int obtainQuantity(){
+        return Integer.parseInt(JOptionPane.showInputDialog(null, "Enter quantity"));
     }
 
-    private double obtainWholeCost() {
-        Double d = null;
-        try {
-            d = Double.parseDouble(JOptionPane.showInputDialog("Enter product whole sale cost"));
-        } catch (NullPointerException ignore) {
-        } finally {
-            return d;
-        }
+    private double obtainWholeCost(){
+        return Double.parseDouble(JOptionPane.showInputDialog("Enter product whole sale cost"));
     }
 
-    private double obtainSalePrice() {
-        Double d = null;
-        try {
-            d = Double.parseDouble(JOptionPane.showInputDialog("Enter product sale price"));
-        } catch (NullPointerException ignore) {
-        } finally {
-            return d;
-        }
+    private double obtainSalePrice(){
+        return Double.parseDouble(JOptionPane.showInputDialog("Enter product sale price"));
     }
 
-    private String obtainSupplierID() {
-        String s = null;
-        try {
-            s = JOptionPane.showInputDialog("Enter supplier id").toUpperCase();
-        } catch (NullPointerException ignore) {
-        } finally {
-            return s;
-        }
+    private String obtainSupplierID(){
+        return JOptionPane.showInputDialog("Enter supplier id").toUpperCase();
     }
 
 
@@ -78,13 +46,11 @@ public class InventoryApp {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String productID = obtainProductID();
-
                 if (!inventory.contains(productID)) {
                     notFound();
                     return;
                 }
                 JOptionPane.showMessageDialog(null, "Product info:\n" + inventory.searchProduct(productID));
-
             }
         });
 
@@ -114,20 +80,12 @@ public class InventoryApp {
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                String productID = null;
-                productID = obtainProductID();
-
-                if (productID == null) {
-                    JOptionPane.showMessageDialog(null, "Error Product does not exist!");
-                    return;
-                }
-
+                String productID = obtainProductID();
                 if (productID.length() != 12) {
                     JOptionPane.showMessageDialog(null, "Invalid product ID");
                     return;
                 }
-                if (inventory.contains(productID)) {
+                if (inventory.contains(productID)){
                     JOptionPane.showMessageDialog(null, "Product already exists");
                     return;
                 }
@@ -141,19 +99,19 @@ public class InventoryApp {
                 }
 
                 String supplierID = obtainSupplierID();
-                if (supplierID.length() != 8) {
-                    JOptionPane.showMessageDialog(null, "Invalid supplier ID");
-                    return;
-                }
-                Product newProduct = new Product(productID, quantity, wholeCost, salePrice, supplierID);
-                inventory.addProduct(newProduct);
-                try {
-                    inventory.update();
-                } catch (FileNotFoundException fileNotFoundException) {
-                    fileNotFoundException.printStackTrace();
-                }
+               if (supplierID.length() != 8) {
+                   JOptionPane.showMessageDialog(null, "Invalid supplier ID");
+                   return;
+               }
+                    Product newProduct = new Product(productID, quantity, wholeCost, salePrice, supplierID);
+                    inventory.addProduct(newProduct);
+                    try {
+                        inventory.update();
+                    } catch (FileNotFoundException fileNotFoundException) {
+                        fileNotFoundException.printStackTrace();
+                    }
 
-                JOptionPane.showMessageDialog(null, "New product " + productID + " has been added");
+                    JOptionPane.showMessageDialog(null, "New product " + productID + " has been added");
             }
         });
         deleteButton.addActionListener(new ActionListener() {
