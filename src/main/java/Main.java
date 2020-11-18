23cc6552476e30cb1dab4e0820c8e6011f40e31c
Dakota Miller <dmill166@msudenver.edu>
2020-11-17 23:49:28 -0700

    daily assets functionality included

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import org.jfree.data.time.Millisecond;
 import org.jfree.ui.RefineryUtilities;
 
 import java.io.IOException;
@@ -5,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalTime;
 
+import static java.time.temporal.ChronoUnit.MILLIS;
 import static java.time.temporal.ChronoUnit.SECONDS;
 
 public class Main {
@@ -12,13 +14,12 @@ public class Main {
         private static LocalTime programStart;
         private static LocalTime programEnd;
         private final String inventory_file = "inventory_team6.csv";
-        private final String customer_orders_file = "10Ktesting.csv"; //file to stress test db.
-        //private final String customer_orders_file = "500Ktesting.csv"; //file to stress test db.
-        //private final String customer_orders_file = "1Mtesting.csv"; //file to stress test db.
-        //private final String customer_orders_file = "customer_orders_A_team6.csv";
+        //private final String customer_orders_file = "10Ktesting.csv"; //file to stress test db.
+        private final String customer_orders_file = "customer_orders_A_team6.csv";
         private final String dim_date_start = "2020-01-01";
         private final String dim_date_end = "2020-12-31";
         private final int resupply_quantity = 500;
+
 
         public Main() {
 
@@ -32,11 +33,10 @@ public class Main {
         }
 
         public void invoke(Credentials credentials,QueryMaker qm) throws IOException, SQLException, ClassNotFoundException {
-                LocalTime cqmCreation, dbCreation, bLoading, bProcessing, chartDemo, mailServiceDemo;
+                LocalTime dbCreation, bLoading, bProcessing, dAssets, chartDemo, mailServiceDemo;
 
                 //Database re-creation as well as batch (.csv) file loading and processing
                 System.out.println("The Credentials and QueryMaker objects have been created.");
-                cqmCreation = LocalTime.now();
 
                 qm.createDatabaseStructure(inventory_file);
                 System.out.println("The basic database structure has been created and inventory has been loaded.");
@@ -49,6 +49,10 @@ public class Main {
                 qm.batchProcessing(resupply_quantity);
                 System.out.println("Batch processing complete.");
                 bProcessing = LocalTime.now();
+
+                qm.generateDailyAssets("01/01/2020", "06/28/2020");
+                System.out.println("Daily assets loaded.");
+                dAssets = LocalTime.now();
 
 
                 //Chart Demonstration
@@ -72,15 +76,16 @@ public class Main {
 //                mail.readEmail(credentials, qm);
 //                System.out.println("Emails read");
 //                System.out.println("Mail Service demonstrations completed.\n");
-                mailServiceDemo = LocalTime.now();
+//                mailServiceDemo = LocalTime.now();
 
                 programEnd = LocalTime.now();
-                System.out.println("\nDatabase authentication/connection took " + programStart.until(cqmCreation, SECONDS) + " seconds.");
-                System.out.println("Database creation took " + cqmCreation.until(dbCreation, SECONDS) + " seconds.");
-                System.out.println("Batch (.csv) loading took " + dbCreation.until(bLoading, SECONDS) + " seconds.");
-                System.out.println("Batch (.csv) processing took " + bLoading.until(bProcessing, SECONDS) + " seconds.");
+                //System.out.println("\nDatabase authentication/connection took " + programStart.until(cqmCreation, SECONDS) + " seconds.");
+                System.out.println("Database creation took " + (programStart.until(dbCreation, MILLIS) / 1000.0) + " seconds.");
+                System.out.println("Batch (.csv) loading took " + (dbCreation.until(bLoading, MILLIS) / 1000.0) + " seconds.");
+                System.out.println("Batch (.csv) processing took " + (bLoading.until(bProcessing, MILLIS) / 1000.0) + " seconds.");
+              System.out.println("Daily assets took " + (bProcessing.until(dAssets, MILLIS) / 1000.0) + " seconds.");
 //                System.out.println("Chart demonstration took " + bProcessing.until(chartDemo, SECONDS) + " seconds.");
-                System.out.println("Mail Service demonstration took " + bProcessing.until(mailServiceDemo, SECONDS) + " seconds.");
-                System.out.println("Total program execution took " + programStart.until(programEnd, SECONDS) + " seconds.");
+                //System.out.println("Mail Service demonstration took " + bProcessing.until(mailServiceDemo, SECONDS) + " seconds.");
+                System.out.println("Total program execution took " + (programStart.until(programEnd, MILLIS) / 1000.0) + " seconds.");
         }
 }
